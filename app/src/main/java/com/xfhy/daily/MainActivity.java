256095d1dfb5e428ddaac5110e802124729ff2d8
xfhy <xfhy666@gmail.com>
2017-09-12 21:16:08 +0800

    init project

---
+++ app/src/main/java/com/xfhy/daily/MainActivity.java
@@ -0,0 +1,18 @@
+package com.xfhy.daily;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * author xfhy
+ * create at 2017/9/12 21:11
+ * description：主界面
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
